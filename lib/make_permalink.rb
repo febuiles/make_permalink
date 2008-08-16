5d92c5b19644e456a6b3bf2f6df900d60382b8d6
Federico Builes <federico.builes@gmail.com>
2008-08-16 12:47:12 -0500

    Initial commit

---
+++ lib/make_permalink.rb
@@ -0,0 +1,28 @@
+module MakePermalink #:nodoc:
+  def self.included(base)
+    base.extend ClassMethods
+  end
+end
+
+module ClassMethods
+  # Creates a permalink for a given object based on the attribute passed as
+  # parameter:
+  #     
+  #   class Post < ActiveRecord::Base
+  #     make_permalink :title
+  #   end
+  #
+  #   p = Post.create(:title => "Hello World!")
+  #   p.permalink   # => "1-hello-world"
+  def make_permalink(method)
+    define_method "permalink" do 
+      field = self.send(method.to_sym) 
+      "#{id}-#{field.gsub(/[^\w]/, "-").downcase}"
+    end
+  end
+end
+
+
+
+
+
