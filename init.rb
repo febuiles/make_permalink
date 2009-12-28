9ec38b9df5dabd1fb262af6ea42e5ce98acdbb24
Federico Builes <federico.builes@gmail.com>
2009-12-28 18:57:56 -0500

    Removing project files, updating README.

--- init.rb
+++
@@ -1,6 +0,0 @@
-#require File.join(File.dirname(__FILE__), "lib", "make_permalink")
-require 'make_permalink'
-
-if Object.const_defined?("ActiveRecord")
-  ActiveRecord::Base.send(:include, MakePermalink)
-end
